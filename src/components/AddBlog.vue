420faf65311c2869acd66a71e8390364209c9df4
unknown <devethancraig@gmail.com>
2019-02-09 20:05:33 -0700

    AddBlog Created and is Submitting Dummy Blogs

---
+++ src/components/AddBlog.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="add-blog">
+    <h2>Add a New Blog Post</h2>
+    <form v-if="!submitted">
+      <label>Blog Title:</label>
+      <input type="text" v-model.lazy="blog.title" required>
+      <label>Blog Content:</label>
+      <textarea v-model.lazy="blog.content"></textarea>
+      <div id="checkboxes">
+        <label>Health</label>
+        <input type="checkbox" value="Health" v-model="blog.categories">
+        <label>Movies</label>
+        <input type="checkbox" value="Movies" v-model="blog.categories">
+        <label>News</label>
+        <input type="checkbox" value="News" v-model="blog.categories">
+        <label>Sports</label>
+        <input type="checkbox" value="Sports" v-model="blog.categories">
+        <label>Tech</label>
+        <input type="checkbox" value="Tech" v-model="blog.categories">
+      </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors" :key="author">{{author}}</option>
+      </select>
+      <button v-on:click.prevent="post">Add Blog</button>
+    </form>
+    <div v-if="submitted">
+      <h3>Thanks for Adding your Post!</h3>
+    </div>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog Title: {{blog.title}}</p>
+      <p>Blog Content:</p>
+      <p>{{blog.content}}</p>
+      <p>Blog Categories:</p>
+      <ul>
+        <li v-for="categorey in blog.categories" :key="categorey">{{categorey}}</li>
+      </ul>
+      <p>Author: {{blog.author}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blog: {
+        title: '',
+        content: '',
+        categories: [],
+        author: ''
+      },
+      authors: ['Myself', 'News', 'Magazine'],
+      submitted: false
+    };
+  },
+  methods: {
+    post: function() {
+      this.$http.post('http://jsonplaceholder.typicode.com/posts', {
+        title: this.blog.title,
+        body: this.blog.content,
+        userId: 1
+      }).then(function(data) {
+        console.log(data)
+        this.submitted = true;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+  #add-blog * {
+    box-sizing: border-box;
+  }
+  #add-blog {
+    margin: 20px auto;
+    max-width: 500px;
+  }
+  label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  input[type="text"],
+  textarea {
+    display: block;
+    width: 100%;
+    padding: 8px;
+  }
+  #preview {
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+  }
+  h3 {
+    margin-top: 10px;
+  }
+  #checkboxes input{
+    display: inline-block;
+    margin-right: 10px;
+  }
+  #checkboxes label{
+      display: inline-block;
+  }
+</style>
